f86fb550c32271b8b4a87cb0c5ef58b65dbbd8d3
linuxdev <asieduernest@yahoo.co.uk>
2024-07-28 01:48:01 -0400

    Update/test: simple test

--- hello_azure/tests.py
+++ hello_azure/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class HomePageTests(TestCase):
+    def setUp(self):
+        self.response = self.client.get('/')
+
+    def test_home_page_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_home_page_contains_welcome(self):
+        self.assertContains(self.response, 'Welcome')
