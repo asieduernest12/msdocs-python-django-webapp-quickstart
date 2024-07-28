5394a4040443434886e0869bd300bd8bec908900
linuxdev <asieduernest@yahoo.co.uk>
2024-07-28 01:48:31 -0400

    Update: use in memory sqlite

--- quickstartproject/settings.py
+++ quickstartproject/settings.py
@@ -81,7 +81,8 @@ WSGI_APPLICATION = 'quickstartproject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': ':memory:',
     }
 }
 
